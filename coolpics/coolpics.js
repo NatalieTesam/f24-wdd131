4e58a2ffe131db10863925da19e70fdb1406ad30
Natalie Tesam <nataliekay1023@gmail.com>
2024-09-28 23:43:58 -0600

    Most of it

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -2,19 +2,19 @@ document.getElementById('year').textContent = new Date().getFullYear();
 
 const menuButton = document.querySelector(".menu-button");
 function toggleMenu() {
-  const menu = document.querySelector(".menu");
-  menu.classList.toggle("hide");
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("hide");
 }
 
 menuButton.addEventListener("click", toggleMenu);
 
 function handleResize() {
-  if (window.innerWidth > 700) {
-    document.querySelector(".menu").classList.remove("hide");
-  }
-  else {
-    document.querySelector(".menu").classList.add("hide");
-  }
+    if (window.innerWidth > 700) {
+        document.querySelector(".menu").classList.remove("hide");
+    }
+    else {
+        document.querySelector(".menu").classList.add("hide");
+    }
 }
 window.addEventListener("resize", handleResize)
 window.addEventListener("load", handleResize)
@@ -27,18 +27,18 @@ function viewerTemplate(image, alt) {
 }
 
 function viewHandler(event) {
-        const element = event.target;
-        const image = element.getAttribute("src").split("-")[0] + "-full.jpeg";
-        const alt = element.getAttribute("alt");
-        const viewer = viewerTemplate(image, alt);
-        document.body.insertAdjacentHTML("afterbegin", viewer);
-        document.querySelector(".close-viewer").addEventListener("click", closeViewer);
-
-        function closeViewer() {
-            document.querySelector(".viewer").remove();
-        }
-
+    const element = event.target;
+    const image = element.getAttribute("src").split("-")[0] + "-full.jpeg";
+    const alt = element.getAttribute("alt");
+    const viewer = viewerTemplate(image, alt);
+    document.body.insertAdjacentHTML("afterbegin", viewer);
 }
 
-document.querySelectorAll(".gallery img").addEventListener("click", viewHandler());
+document.querySelector(".close-viewer").addEventListener("click", closeViewer);
+
+function closeViewer() {
+    document.querySelector(".viewer").remove();
+}
+
+document.querySelectorAll(img).addEventListener("click", viewHandler());
 
